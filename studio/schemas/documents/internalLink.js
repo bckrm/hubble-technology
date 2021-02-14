da2f53e912d530a231cdfb08dcaf4d63e7820934
asasmith <smith.asa.la@gmail.com>
2021-02-14 17:13:00 -0500

    Add internal link document

---
+++ studio/schemas/documents/internalLink.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'link',
+    title: 'Link',
+    type: 'document',
+    fields: [
+        {
+            name: 'text',
+            title: 'Link Text',
+            type: 'string',
+        },
+        {
+            name: 'url',
+            title: 'URL',
+            type: 'string',
+        },
+    ],
+};
