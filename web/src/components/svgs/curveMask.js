a25c57c0cc1e92ad4bfa613c4729227253301411
asasmith <smith.asa.la@gmail.com>
2021-02-14 23:06:46 -0500

    Add isInverted prop

--- web/src/components/svgs/curveMask.js
+++ web/src/components/svgs/curveMask.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -7,11 +8,15 @@ const Svg = styled.svg`
     ${tw`absolute w-full`}
 
     top: ${({ isTop }) => (isTop ? '0' : '100%')};
-    transform: ${({ isTop }) =>
-        isTop ? 'translateY(-50%)' : 'translateY(-50%) scale(-1)'};
+    transform: ${({ isInverted, isTop }) =>
+        isTop && isInverted
+            ? 'translateY(-50%) scale(-1)'
+            : isTop
+            ? 'translateY(-50%)'
+            : 'translateY(-50%) scale(-1)'};
 `;
 
-export default function CurveMask({ color, isTop }) {
+export default function CurveMask({ color, isTop, isInverted }) {
     return (
         <Svg
             width="1440"
@@ -19,6 +24,7 @@ export default function CurveMask({ color, isTop }) {
             viewBox="0 0 1440 252"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            isInverted={isInverted}
             isTop={isTop}
         >
             <path
@@ -31,5 +37,6 @@ export default function CurveMask({ color, isTop }) {
 
 CurveMask.propTypes = {
     color: PropTypes.string.isRequired,
+    isInverted: PropTypes.bool.isRequired,
     isTop: PropTypes.bool.isRequired,
 };
