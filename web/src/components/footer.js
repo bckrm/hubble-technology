952e8b76f6934d6bdb9243590e6d0a893f88c77d
asasmith <smith.asa.la@gmail.com>
2021-02-20 21:33:52 -0500

    Update footer

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -1,60 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import tw from 'twin.macro';
+import { Link } from 'gatsby';
 
 import TextLogoWhite from './logos/textLogoWhite';
 
+const StyledFooter = styled.footer`
+    ${tw`bg-black-1 py-14 text-white`}
+`;
+
+const FooterInner = styled.div`
+    ${tw`container flex justify-between flex-col md:flex-row`}
+`;
+
+const FooterNav = styled.div`
+    ${tw`flex flex-col md:flex-row items-center md:items-start`}
+`;
+
+const ContactDiv = styled.div`
+    ${tw`flex flex-col mb-10 md:mr-20`}
+`;
+
+const LogoLink = styled(Link)`
+    ${tw`mb-3`}
+`;
+
+const UL = styled.ul`
+    ${tw`grid grid-cols-2 gap-x-10 mb-10 md:mb-0`}
+`;
+
+// link hover styles added as className, need to revisit
+// link hover state utility classes applied using className in jsx
+
+const Form = styled.form`
+    ${tw`flex flex-col items-center md:items-end`}
+`;
+
+const FormHeading = styled.p`
+    ${tw`capitalize font-bold mb-3.5 text-xl text-right`}
+`;
+
+const Label = styled.label`
+    ${tw`sr-only`}
+`;
+
+const Input = styled.input`
+    ${tw`bg-white border border-white mb-5 p-1 text-lg text-right`}
+`;
+
+const Button = styled.button`
+    ${tw`bg-green self-center md:self-end capitalize py-2 px-6 max-w-max text-xl tracking-widest`}
+`;
+
+// todo: confirm hover states for internal links
+// todo: wire up form submission
+
 const content = {
-    footerLinks: [
+    internalLinks: [
         {
-            name: 'LinkedIn',
-            link: 'https://www.linkedin.com/company/hubble-technology/',
+            link: '/#',
+            text: 'Link',
         },
         {
-            name: 'Twitter',
-            link: 'https://twitter.com/gethubble',
+            link: '/#',
+            text: 'Link',
+        },
+        {
+            link: '/#',
+            text: 'Link',
+        },
+        {
+            link: '/#',
+            text: 'Link',
         },
     ],
+    form: {
+        heading: 'newsletter',
+        buttonText: 'submit',
+    },
 };
 
-const StyledFooter = styled.footer`
-    ${tw`container flex flex-col md:flex-row justify-between mx-auto px-4 lg:px-2 2xl:px-0`}
-`;
-
-const LogoWrapper = styled.div`
-    ${tw`mb-12 md:mb-0 self-center md:self-start`}
-`;
-
-const StyledUL = styled.ul`
-    ${tw`flex flex-wrap justify-center md:justify-start mx-auto md:mx-0 w-1/2`}
-
-    @media screen and (min-width: 768px) {
-        max-width: 16rem;
-    }
-`;
-
-const StyledLink = styled.a`
-    ${tw`mx-4 md:mx-0 md:mr-12`}
-`;
-
 export default function Footer() {
+    const [email, setEmail] = useState('');
+    const handleChange = ({ target }) => {
+        const { value } = target;
+        setEmail(value);
+    };
+    const links = content.internalLinks.map((item) => {
+        return (
+            <li key={item.text}>
+                <Link to={item.link}>{item.text}</Link>
+            </li>
+        );
+    });
+
     return (
         <StyledFooter>
-            <LogoWrapper>
-                <TextLogoWhite />
-            </LogoWrapper>
-            <StyledUL>
-                {content.footerLinks.map((item) => {
-                    return (
-                        <li key={item.name}>
-                            <StyledLink href={item.link} target="_blank">
-                                {item.name}
-                            </StyledLink>
-                        </li>
-                    );
-                })}
-            </StyledUL>
+            <FooterInner>
+                <FooterNav>
+                    <ContactDiv>
+                        <LogoLink to="/">
+                            <TextLogoWhite />
+                        </LogoLink>
+                    </ContactDiv>
+                    <UL>{links}</UL>
+                </FooterNav>
+                <Form>
+                    <FormHeading>{content.form.heading}</FormHeading>
+                    <Label htmlFor="email sr-only">Email</Label>
+                    <Input
+                        name="email"
+                        placeholder="email"
+                        type="email"
+                        value={email}
+                        onChange={handleChange}
+                    />
+                    <Button type="submit">{content.form.buttonText}</Button>
+                </Form>
+            </FooterInner>
         </StyledFooter>
     );
 }
