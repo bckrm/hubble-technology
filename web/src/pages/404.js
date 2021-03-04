a04a5874fd34649c5f26308fa030ca84811b1b1c
kcbreuil <kcbreuil@gmail.com>
2021-03-03 22:36:50 -0500

    more work on 404

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,53 +1,56 @@
 import * as React from 'react';
-import { graphql } from 'gatsby';
-
-// import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import styled from 'styled-components';
+import Img from 'gatsby-image';
 import tw from 'twin.macro';
 import PropTypes from 'prop-types';
 
 import Layout from '../page-layouts/layout';
 import Hero from '../components/hero';
+import CtaSection from '../components/ctaSection';
 
-// const Main = styled.main`
-//     ${tw`container`}
-
-//     color: '#232129',
-//     fontFamily: '-apple-system, Roboto, sans-serif, serif',
-// `;
-
-// const content = {
-//     hero: `Have you lost your way?`,
-//     body: `Let's get you back home!`,
-// };
+const Wrapper = styled.div`
+    ${tw`flex flex-col justify-center items-center content-center`}
+`;
 
 const StyledHeading = styled.h2`
-    ${tw`font-bold text-5xl mb-12`}
+    ${tw`font-bold text-center text-5xl w-2/4 mb-4`}
 
     line-height: 3.75rem;
     max-width: 58rem;
 `;
 
 const StyledP = styled.p`
-    ${tw`font-semibold md:max-w-3xl pb-16 lg:pb-24 text-2xl`}
+    ${tw`text-2xl mb-28`}
 
     line-height: 150%;
 `;
 
 // markup
 export default function NotFoundPage({ data }) {
-    const { hero } = data;
+    const { cta, fourohfour, hero } = data;
     return (
         <Layout>
             <Hero content={hero} />
-            <StyledHeading>Are you lost?</StyledHeading>
-            <StyledP>Let&apos;s get you back home </StyledP>
+            <Wrapper>
+                <Img fluid={fourohfour.childImageSharp.fluid} />
+                <StyledHeading>
+                    Didn’t find what you’re looking for?
+                </StyledHeading>
+                <StyledP>
+                    This page doesn’t exist, click <Link to="/">here</Link> to
+                    head back to the homepage{' '}
+                </StyledP>
+            </Wrapper>
+            <CtaSection content={cta} />
         </Layout>
     );
 }
 
 NotFoundPage.propTypes = {
     data: PropTypes.shape({
+        cta: PropTypes.object.isRequired,
+        fourohfour: PropTypes.object.isRequired,
         hero: PropTypes.object.isRequired,
     }).isRequired,
 };
@@ -64,6 +67,20 @@ export const query = graphql`
                 }
             }
             heading: heroHeading
+        }
+        cta: sanityAboutPage {
+            ctaHeading
+            link {
+                text
+                url
+            }
+        }
+        fourohfour: file(relativePath: { regex: "/404/" }) {
+            childImageSharp {
+                fluid(maxWidth: 500) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
         }
     }
 `;
