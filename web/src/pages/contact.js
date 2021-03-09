7e9c484a6024ef73eb5679273b2449a0e21ec283
kcbreuil <kcbreuil@gmail.com>
2021-03-09 17:04:32 -0500

    query example

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -72,6 +72,7 @@ export const query = graphql`
         }
         industries: sanityIndexPage {
             heading: industriesHeading
+            description: industriesHeading
             indexIndustries {
                 id
                 industryName
