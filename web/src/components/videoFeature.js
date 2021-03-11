e5f73873b78fe9c678d5678c149b0c5725082316
kcbreuil <kcbreuil@gmail.com>
2021-03-11 11:12:09 -0500

    fix mobile view on video feature

--- web/src/components/videoFeature.js
+++ web/src/components/videoFeature.js
@@ -5,20 +5,51 @@ import Img from 'gatsby-image';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
+// const GridWrapper = styled.div`
+//     ${tw`lg:grid lg:grid-cols-2 lg:grid-rows-1 my-16 h-5/6 bg-gray-1`}
+// `;
+
+// const ContentWrapper = styled.div`
+//     ${tw`lg:col-start-2 col-span-1 lg:w-9/12 mx-4 justify-self-center self-center`}
+// `;
+
+// const StyledImg = styled(Img)`
+//     ${tw`col-start-1 col-span-1 lg:my-20 lg:w-8/12`}
+// `;
+
+// const StyledHeading = styled.h3`
+//     ${tw`font-bold text-xl mb-8 leading-6 relative lg:text-3xl lg:w-7/12`}
+
+//     &:after {
+//         content: '';
+//         position: absolute;
+//         width: 1.5rem;
+//         left: 0;
+//         bottom: -0.75rem;
+//         transform: translateY(100%);
+//         height: 0.25rem;
+//         background-color: #00a09b;
+//     }
+// `;
+
+// const StyledP = styled.p`
+//     ${tw`leading-6 text-xs lg:text-xl`}
+// `;
+
 const GridWrapper = styled.div`
-    ${tw`grid grid-cols-2 grid-rows-1 my-16 h-5/6 bg-gray-1`}
+    ${tw`flex flex-col lg:grid grid-cols-2 grid-rows-1 mx-4 lg:my-16 lg:h-5/6 bg-gray-1`}
 `;
 
 const ContentWrapper = styled.div`
-    ${tw`col-start-2 col-span-1 w-9/12 justify-self-center self-center`}
+    ${tw`col-start-2 col-span-1 lg:w-9/12 justify-self-center self-center`}
 `;
 
 const StyledImg = styled(Img)`
-    ${tw`col-start-1 col-span-1 my-20 w-8/12`}
+    ${tw`lg:col-start-1 col-span-1 my-7 mb-32 lg:my-20 w-auto`}
 `;
 
 const StyledHeading = styled.h3`
-    ${tw`font-bold leading-8 mb-8 relative text-3xl w-7/12`}
+    ${tw`font-bold leading-8 my-8 relative text-3xl w-7/12`}
     &:after {
         content: '';
         position: absolute;
