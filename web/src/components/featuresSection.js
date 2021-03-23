2809203cf63b38678b77c44989680243c9d3b289
kcbreuil <kcbreuil@gmail.com>
2021-03-23 14:25:14 -0400

    removed object content

--- web/src/components/featuresSection.js
+++ web/src/components/featuresSection.js
@@ -6,26 +6,7 @@ import tw from 'twin.macro';
 
 import Feature from './feature';
 
-const content = {
-    heading: '',
-    features: [
-        {
-            heading: 'Compliance and Security',
-            body:
-                'Ensure compliance and security across the entire organization. Infuse technology asset technology into your existing security investments, enabling enhanced situational awareness and super-charging security automation and orchestration.',
-        },
-        {
-            heading: 'Digital Transformation',
-            body:
-                'Enable digital transformation with real time asset data that helps you identify opportunities for technology rationalization, innovation and transformation.',
-        },
-        {
-            heading: 'Untrap Value',
-            body:
-                'Releasing trapped value is key to success in a digital world. Our platform is designed to unlock the value of your existing technology data to help power your business through asset intelligence.',
-        },
-    ],
-};
+const content = {};
 
 const StyledSection = styled.section`
     ${tw`container mx-auto pt-24 lg:pt-36 px-4 lg:px-2 2xl:px-0`}
