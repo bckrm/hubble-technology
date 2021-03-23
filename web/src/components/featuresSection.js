74e10369da7b6e335a4f987d0ad5f441a923cb35
kcbreuil <kcbreuil@gmail.com>
2021-03-23 15:43:42 -0400

    fix error by putting object back

--- web/src/components/featuresSection.js
+++ web/src/components/featuresSection.js
@@ -6,7 +6,26 @@ import tw from 'twin.macro';
 
 import Feature from './feature';
 
-const content = {};
+const content = {
+    heading: 'Why Hubble?',
+    features: [
+        {
+            heading: 'Compliance and Security',
+            body:
+                'Ensure compliance and security across the entire organization. Infuse technology asset technology into your existing security investments, enabling enhanced situational awareness and super-charging security automation and orchestration.',
+        },
+        {
+            heading: 'Digital Transformation',
+            body:
+                'Enable digital transformation with real time asset data that helps you identify opportunities for technology rationalization, innovation and transformation.',
+        },
+        {
+            heading: 'Untrap Value',
+            body:
+                'Releasing trapped value is key to success in a digital world. Our platform is designed to unlock the value of your existing technology data to help power your business through asset intelligence.',
+        },
+    ],
+};
 
 const StyledSection = styled.section`
     ${tw`container mx-auto pt-24 lg:pt-36 px-4 lg:px-2 2xl:px-0`}
