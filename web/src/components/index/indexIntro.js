88067bb11a7c725b104d38411f6a8dd4a389fd7d
kcbreuil <kcbreuil@gmail.com>
2021-03-24 13:31:13 -0400

    fix

--- web/src/components/index/indexIntro.js
+++ web/src/components/index/indexIntro.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 import { motion, AnimatePresence } from 'framer-motion';
 
-// import Img from 'gatsby-image';
 import BlockText from '../block-content/blockText';
 import IndexAnimation from '../indexAnimation';
 
@@ -13,22 +12,23 @@ const Section = styled.section`
 `;
 
 const Wrapper = styled.div`
-    ${tw`relative mt-20 lg:absolute lg:left-full lg:top-full lg:transform-gpu lg:-translate-y-full lg:-translate-x-full lg:w-1/2`}
-
-    //todo move colors to tailwind config
-    &:after {
-        background: #00a09b;
-        content: '';
-        height: 0.25rem;
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 1.5rem;
-    }
+    ${tw`relative mt-20 lg:absolute lg:left-full lg:top-full lg:transform-gpu lg:-translate-y-full lg:-translate-x-full lg:w-1/2`}//todo move colors to tailwind config
 `;
 
 const Heading = styled.h2`
     ${tw`font-bold text-4xl mb-12`}
+`;
+
+const TextWrapper = styled.div`
+    &:after {
+        background: #00a09b;
+        content: '';
+        position: absolute;
+        height: 0.25rem;
+        top: 40%;
+        transform: translateY(-50%);
+        width: 1.5rem;
+    }
 `;
 
 export default function IndexIntro({ content }) {
@@ -48,7 +48,9 @@ export default function IndexIntro({ content }) {
                                 duration: 2,
                             }}
                         >
-                            <BlockText blocks={_rawIntroBody} />
+                            <TextWrapper>
+                                <BlockText blocks={_rawIntroBody} />
+                            </TextWrapper>
                         </motion.div>
                     )}
                 </AnimatePresence>
