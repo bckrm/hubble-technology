ebfc3d9d6ae1075af3df4075f680cc9147716dcd
Katie Breuil <61708936+kcbreuil@users.noreply.github.com>
2021-03-25 10:10:06 -0400

    Add homepage animation (#50)

--- web/src/components/index/indexIntro.js
+++ web/src/components/index/indexIntro.js
@@ -1,48 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import { motion, AnimatePresence } from 'framer-motion';
 
-import Img from 'gatsby-image';
 import BlockText from '../block-content/blockText';
+import IndexAnimation from '../indexAnimation';
 
 const Section = styled.section`
     ${tw`container relative`}
 `;
 
 const Wrapper = styled.div`
-    ${tw`relative lg:absolute lg:left-full lg:top-full lg:transform-gpu lg:-translate-y-full lg:-translate-x-full lg:w-1/2`}
-
+    ${tw`relative mt-20 lg:absolute lg:left-full lg:top-full lg:transform-gpu lg:-translate-y-full lg:-translate-x-full lg:w-1/2`}
     //todo move colors to tailwind config
-    &:after {
-        background: #00a09b;
-        content: '';
-        height: 0.25rem;
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 1.5rem;
+    .motion-wrapper {
+        position: relative;
+
+        &:after {
+            background: #00a09b;
+            content: '';
+            height: 0.25rem;
+            position: absolute;
+            top: 0;
+            transform: translateY(-1.5rem);
+            width: 1.5rem;
+        }
     }
 `;
 
 const Heading = styled.h2`
-    ${tw`font-bold text-4xl mb-12`}
+    ${tw`font-bold text-2xl md:text-4xl mb-12`}
 `;
 
 export default function IndexIntro({ content }) {
-    const {
-        introHeading,
-        introPlaceholderImage: {
-            asset: { fluid: imageData },
-        },
-        _rawIntroBody,
-    } = content;
+    const [isFinishedAnimation, setIsFinishedAnimation] = useState(false);
+    const { introHeading, _rawIntroBody } = content;
     return (
         <Section>
-            <Img fluid={imageData} />
+            <IndexAnimation setIsFinishedAnimation={setIsFinishedAnimation} />
             <Wrapper>
                 <Heading>{introHeading}</Heading>
-                <BlockText blocks={_rawIntroBody} />
+                <AnimatePresence>
+                    {isFinishedAnimation && (
+                        <motion.div
+                            className="motion-wrapper"
+                            initial={{ opacity: 0, height: 0 }}
+                            animate={{ opacity: 1, height: 'auto' }}
+                            transition={{
+                                duration: 2,
+                            }}
+                        >
+                            <BlockText blocks={_rawIntroBody} />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </Wrapper>
         </Section>
     );
@@ -51,7 +63,6 @@ export default function IndexIntro({ content }) {
 IndexIntro.propTypes = {
     content: PropTypes.shape({
         introHeading: PropTypes.string.isRequired,
-        introPlaceholderImage: PropTypes.object.isRequired,
         _rawIntroBody: PropTypes.array.isRequired,
     }).isRequired,
 };
