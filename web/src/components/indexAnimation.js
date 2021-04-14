9286f42f55948fc8d614c4e481d8e2ee013d7d5c
kcbreuil <kcbreuil@gmail.com>
2021-04-14 14:08:02 -0400

    animation check

--- web/src/components/indexAnimation.js
+++ web/src/components/indexAnimation.js
@@ -3,7 +3,8 @@ import Lottie from 'lottie-react';
 import PropTypes from 'prop-types';
 import { useInView } from 'react-intersection-observer';
 
-import hubble from '../animations/hubble_newnewedit.json';
+// import hubble from '../animations/hubble_newnewedit.json';
+import hubble from '../animations/Homepage-Edit.json';
 
 export default function IndexAnimation({ setIsFinishedAnimation }) {
     const indexContainer = useRef(null);
